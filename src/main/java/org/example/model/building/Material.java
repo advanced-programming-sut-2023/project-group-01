5585725046ab09c37323e75855612541394f42aa
SHAYAN SYSTEM <lyqasmzadh72@gmail.com>
2023-04-20 12:51:20 +0430

    make materialclass and enum

--- src/main/java/org/example/model/building/Material.java
+++ src/main/java/org/example/model/building/Material.java
@@ -1,4 +1,9 @@
 package org.example.model.building;
 
 public class Material {
+    private org.example.model.building.MaterialType materialType;
+
+    public Material(org.example.model.building.MaterialType materialType) {
+        this.materialType = materialType;
+    }
 }
