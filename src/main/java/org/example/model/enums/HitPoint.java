e4f8c92c706efc66b3c8cd4e884de5ed17628f6c
SHAYAN SYSTEM <lyqasmzadh72@gmail.com>
2023-04-20 12:42:04 +0430

    change hitPoint class

--- src/main/java/org/example/model/enums/HitPoint.java
+++ src/main/java/org/example/model/enums/HitPoint.java
@@ -5,12 +5,8 @@ public enum HitPoint {
     UNIT_MIDDLE(120),
     UNIT_LOW(60),
     UNIT_SO_LOW(40),
-    UNIT_NOTHING(20),
-    //Model.building.Building HitPoint :
-    BUILDING_HIGH(1500),
-    BUILDING_MIDDLE(1000),
-    BUILDING_LOW(500),
-    BUILDING_NOTHING(100);
+    UNIT_NOTHING(20);
+
     private int hitPoint;
 
     HitPoint(int hitPoint) {
