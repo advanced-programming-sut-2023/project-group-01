c13876a2e65ddb6779fb7336a7a5d2191b7f4e78
Saeed Forati K <saeed.forati.k@gmail.com>
2023-04-24 12:06:16 +0330

    Add new outputs and extracted password hash generator function.

--- src/main/java/org/example/view/enums/Outputs.java
+++ src/main/java/org/example/view/enums/Outputs.java
@@ -2,7 +2,7 @@ package org.example.view.enums;
 
 public enum Outputs {
 
-    NOT_ENOUGH_DATA("Your entered data is not enough for registering new user."),
+    NOT_ENOUGH_DATA("Your entered data is not enough for this command."),
     INVALID_USERNAME("Your entered username is invalid."),
     USER_EXISTS("There is a user existing with your entered username."),
     SHORT_PASSWORD("Your entered password is short."),
@@ -14,7 +14,20 @@ public enum Outputs {
     WRONG_PASSWORD_CONFIRM("Your password confirmation does not match with password."),
     EMAIL_EXISTS("There is a user existing with your entered email."),
     INVALID_EMAIL("Your entered email is invalid."),
-    VALID_REGISTRATION_INPUT("valid");
+    VALID_REGISTRATION_INPUT("valid"),
+    INVALID_REGISTRATION_INPUT("Invalid input for registration."),
+    RANDOM_PASSWORD_CONFIRMATION("Your random password is : "),
+    INVALID_QUESTION_NUMBER("Your entered question number is invalid."),
+    WRONG_ANSWER_CONFIRM("Your entered answer doesn't match with answer confirm."),
+    SUCCESS("success"),
+    RANDOM_SLOGAN("Your random slogan is : "),
+    VALID_LOGIN_INPUT("valid"),
+    INVALID_LOGIN_INPUT("Invalid input for login."),
+    NOT_EXISTING_USERNAME("Your entered username doesn't exist."),
+    WRONG_PASSWORD("Your entered password is wrong."),
+    WRONG_ANSWER("Your security answer is wrong."),
+    INVALID_FORGET_PASSWORD_INPUT("invalid input for password forget menu."),
+    PASSWORD_CHANGE_SUCCESSFUL("Your password changed successfully.");
     private String output;
 
     Outputs(String output) {
