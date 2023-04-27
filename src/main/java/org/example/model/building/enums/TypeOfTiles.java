ebc913b0d8aea105bdcd24b54e3030bc8a022b6a
SHAYAN SYSTEM <lyqasmzadh72@gmail.com>
2023-04-27 17:19:34 +0430

    COMPLETE TYPE OF TILES

---
+++ src/main/java/org/example/model/building/enums/TypeOfTiles.java
@@ -0,0 +1,11 @@
+package org.example.model.building.enums;
+
+public enum TypeOfTiles {
+    NORMAL_GROUND,
+    GRAVEL_GROUND,
+    ROCKY_GROUND,
+    IRON_MINE,
+    GRASSLAND,
+    MEADOW,
+    FULL_MEADOW,
+}
