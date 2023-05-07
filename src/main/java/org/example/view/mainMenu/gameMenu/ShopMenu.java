94001774baec7aad1f6284e1e0bddf859a5996f4
Saeed Forati K <saeed.forati.k@gmail.com>
2023-05-07 14:38:14 +0330

    Edit printing price list

--- src/main/java/org/example/view/mainMenu/gameMenu/ShopMenu.java
+++ src/main/java/org/example/view/mainMenu/gameMenu/ShopMenu.java
@@ -1,7 +1,7 @@
 package org.example.view.mainMenu.gameMenu;
 
 import org.example.model.building.Material;
-import org.example.model.building.enums.MaterialType;
+import org.example.view.enums.BackgroundColor;
 import org.example.view.enums.Outputs;
 import org.example.view.enums.commands.GameMenuCommands.ShopMenuCommands;
 
@@ -17,36 +17,39 @@ public class ShopMenu {
         String inputLine;
         Matcher matcher;
 
-        while (true){
+        while (true) {
             inputLine = scanner.nextLine();
 
-            if (ShopMenuCommands.getMatcher(inputLine,ShopMenuCommands.SHOW_PRICE_LIST).find())
-                System.out.println(showPriceList());
-            else if ((matcher=ShopMenuCommands.getMatcher(inputLine,ShopMenuCommands.BUY)).find())
+            if (ShopMenuCommands.getMatcher(inputLine, ShopMenuCommands.SHOW_PRICE_LIST).find())
+                showPriceList();
+            else if ((matcher = ShopMenuCommands.getMatcher(inputLine, ShopMenuCommands.BUY)).find())
                 System.out.println(buy(matcher).toString());
-            else if ((matcher=ShopMenuCommands.getMatcher(inputLine,ShopMenuCommands.SELL)).find())
+            else if ((matcher = ShopMenuCommands.getMatcher(inputLine, ShopMenuCommands.SELL)).find())
                 System.out.println(sell(matcher).toString());
-            else if (inputLine.equals("exit shop menu")){
+            else if (inputLine.equals("show golds"))
+                System.out.println("You have " + GameMenu.getThisEmpire().getGold() + " golds.");
+            else if (inputLine.equals("exit shop menu")) {
                 return;
-            }
-            else System.out.println("Invalid command in Shop Menu");
+            } else System.out.println("Invalid command in Shop Menu");
 
         }
     }
 
-    public Outputs buy(Matcher matcher){
+    public Outputs buy(Matcher matcher) {
 
         String name = matcher.group("name");
         int count = Integer.parseInt(matcher.group("count"));
 
-        LinkedHashMap<Material,Integer> materials = GameMenu.getThisEmpire().getMaterials();
+        LinkedHashMap<Material, Integer> materials = GameMenu.getThisEmpire().getMaterials();
 
-        for (Material material : materials.keySet()){
-            if (material.getMaterialType().getName().equals(name)){
-                if (GameMenu.getThisEmpire().getGold()<material.getMaterialType().getBuyingPrice()*count)
+        for (Material material : materials.keySet()) {
+            if (material.getMaterialType().getName().equals(name)) {
+                if (material.getMaterialType().getSellingPrice() == 0)
+                    return Outputs.INVALID_MATERIAL_NAME;
+                if (GameMenu.getThisEmpire().getGold() < material.getMaterialType().getBuyingPrice() * count)
                     return Outputs.NOT_ENOUGH_GOLD;
-                materials.put(material,materials.get(material)+count);
-                GameMenu.getThisEmpire().decreaseGold(material.getMaterialType().getBuyingPrice()*count);
+                materials.put(material, materials.get(material) + count);
+                GameMenu.getThisEmpire().decreaseGold(material.getMaterialType().getBuyingPrice() * count);
                 return Outputs.SUCCESS_BUY;
             }
         }
@@ -54,19 +57,21 @@ public class ShopMenu {
         return Outputs.INVALID_MATERIAL_NAME;
     }
 
-    public Outputs sell(Matcher matcher){
+    public Outputs sell(Matcher matcher) {
 
         String name = matcher.group("name");
         int count = Integer.parseInt(matcher.group("count"));
 
-        LinkedHashMap<Material,Integer> materials = GameMenu.getThisEmpire().getMaterials();
+        LinkedHashMap<Material, Integer> materials = GameMenu.getThisEmpire().getMaterials();
 
-        for (Material material : materials.keySet()){
-            if (material.getMaterialType().getName().equals(name)){
-                if (materials.get(material)<count)
+        for (Material material : materials.keySet()) {
+            if (material.getMaterialType().getName().equals(name)) {
+                if (material.getMaterialType().getSellingPrice() == 0)
+                    return Outputs.INVALID_MATERIAL_NAME;
+                if (materials.get(material) < count)
                     return Outputs.NOT_ENOUGH_MATERIAL;
-                materials.put(material,materials.get(material)-count);
-                GameMenu.getThisEmpire().increaseGold(material.getMaterialType().getBuyingPrice()*count);
+                materials.put(material, materials.get(material) - count);
+                GameMenu.getThisEmpire().increaseGold(material.getMaterialType().getBuyingPrice() * count);
                 return Outputs.SUCCESS_SELL;
             }
         }
@@ -74,15 +79,19 @@ public class ShopMenu {
         return Outputs.INVALID_MATERIAL_NAME;
     }
 
-    public String showPriceList(){
-        String output="";
+    public void showPriceList() {
 
-        for (Material material : GameMenu.getThisEmpire().getMaterials().keySet()){
-            output+="Material : "+material.getMaterialType().getName()+" | Buying Price : "+material.getMaterialType().getBuyingPrice()
-                    +" | Selling  Price : "+material.getMaterialType().getSellingPrice()+" | inventory : "
-                    +GameMenu.getThisEmpire().getMaterials().get(material)+"\n";
+        BackgroundColor.changeColor(BackgroundColor.ANSI_GREEN_BACKGROUND);
+        BackgroundColor.changeColor(BackgroundColor.ANSI_BLACK_TEXT);
+        System.out.println("-------------------------------------------------------------------------------");
+        System.out.format("%15s %20s %20s %15s\n","Material","Buying Price","Selling Price","Inventory");
+        System.out.println("-------------------------------------------------------------------------------");
+        BackgroundColor.changeColor(BackgroundColor.ANSI_BLUE_BACKGROUND);
+        for (Material material : GameMenu.getThisEmpire().getMaterials().keySet()) {
+            if (material.getMaterialType().getSellingPrice() != 0)
+                System.out.format("%15s %16s %17s %18s\n",material.getMaterialType().getName(),material.getMaterialType().getBuyingPrice(),material.getMaterialType().getSellingPrice(),GameMenu.getThisEmpire().getMaterials().get(material));
         }
+        BackgroundColor.changeColor(BackgroundColor.ANSI_RESET);
 
-        return output;
     }
 }
